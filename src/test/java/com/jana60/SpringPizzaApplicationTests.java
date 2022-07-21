2e546cda89a95bd17f4c37075213f70dbae2d50e
simmasa <s.masa@hotmail.it>
2022-07-21 13:45:29 +0200

    File creati

---
+++ src/test/java/com/jana60/SpringPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jana60;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
