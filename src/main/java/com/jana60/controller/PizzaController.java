319e8c0a7656f4e06795089b47947af8acff659c
simmasa <s.masa@hotmail.it>
2022-07-22 14:03:31 +0200

    aggiunta funzione delete

--- src/main/java/com/jana60/controller/PizzaController.java
+++ src/main/java/com/jana60/controller/PizzaController.java
@@ -3,15 +3,16 @@ package com.jana60.controller;
 import com.jana60.model.Pizza;
 import com.jana60.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
@@ -40,5 +41,17 @@ public class PizzaController {
             return "redirect:/";
         }
     }
+    @GetMapping("/delete/{id}")
+    public String PizzaDel (@PathVariable("id") Integer pizid , RedirectAttributes rd) {
+        Optional<Pizza> pizDel = repo.findById(pizid);
+        if (pizDel.isPresent()) {
+            repo.deleteById(pizid);
+            rd.addFlashAttribute("deleted","L'elemento è stato eliminato");
+            return "redirect:/";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "La pizza " + pizid + " non esiste");
+        }
 
+    }
 }
